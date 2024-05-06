bae4086a4f202eb49cf933a6d34bdacd9bc23432
Michael <mpierson00@gmail.com>
2024-05-05 21:53:09 -0400

    fixed error in category model

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+console.log('Database:', process.env.DB_NAME);
+console.log('User:', process.env.DB_USER);
+console.log('Password:', process.env.DB_PASSWORD ? '****' : 'Not Provided');
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
